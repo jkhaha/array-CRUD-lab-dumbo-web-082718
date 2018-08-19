af5c170b0005ebfc0b8a6fee6e19618b7f1a1058
Jackie Ha <jha2@wellesley.edu>
2018-08-19 02:13:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-     add_element_to_start_of_array.unshift"wow"
+     add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
